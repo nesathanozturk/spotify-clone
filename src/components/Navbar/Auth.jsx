dd555f29a9890c929c0b494999bbed5539b0f11a
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-08 17:12:44 +0300

    feat: created dropdown

--- src/components/Navbar/Auth.jsx
+++ src/components/Navbar/Auth.jsx
@@ -1,5 +1,78 @@
+import { Menu } from "@headlessui/react";
+import { Icon } from "Icons";
+
 function Auth() {
-  return <nav>auth</nav>;
+  const user = {
+    name: "Neşathan Öztürk",
+    avatar: "https://avatars.githubusercontent.com/u/107864503?v=4",
+  };
+
+  return (
+    <Menu as="nav" className={"relative"}>
+      {({ open }) => (
+        <>
+          <Menu.Button
+            className={`flex items-center h-8 rounded-3xl bg-black pr-2 hover:bg-active ${
+              open ? "bg-active" : "bg-black"
+            }`}
+          >
+            <img
+              src={user.avatar}
+              alt="Avatar"
+              className={"w-8 h-8 rounded-full p-0.5 mr-2"}
+            />
+            <span className="text-sm font-semibold mr-2">{user.name}</span>
+            <span className={open && "rotate-180"}>
+              <Icon name="downDir" size={16} />
+            </span>
+          </Menu.Button>
+          <Menu.Items
+            className={
+              "absolute p-1 top-full right-0 w-48 bg-active rounded translate-y-2"
+            }
+          >
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  className={`h-10 flex justify-between items-center px-2 text-sm rounded ${
+                    active && "bg-white bg-opacity-10"
+                  }`}
+                  href="/s"
+                >
+                  Account
+                  <Icon name="external" size={16} />
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  className={`h-10 flex items-center px-2 text-sm rounded ${
+                    active && "bg-white bg-opacity-20"
+                  }`}
+                  href="/s"
+                >
+                  Profile
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  className={`h-10 flex items-center px-2 text-sm rounded ${
+                    active && "bg-white bg-opacity-20"
+                  }`}
+                  href="/s"
+                >
+                  Log out
+                </a>
+              )}
+            </Menu.Item>
+          </Menu.Items>
+        </>
+      )}
+    </Menu>
+  );
 }
 
 export default Auth;

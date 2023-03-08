a7e6d3b0a4a052c22f30dede41c60cbb3dc74029
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-08 13:48:07 +0300

    feat: added router

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,16 @@
 import Navbar from "./Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "components/views/Home";
+import Search from "components/views/Search";
 
 function Content() {
   return (
     <div className="flex flex-auto">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </div>
   );
 }

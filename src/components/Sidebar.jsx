38847c2614c34a9b61e591c7366da7c2bca105d7
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-08 14:15:35 +0300

    feat: created playlits

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import logo from "img/logo.svg";
 import Menu from "components/Sidebar/Menu";
+import Playlists from "components/Sidebar/Playlists";
+import { Icon } from "Icons";
 
 function Sidebar() {
   return (
@@ -8,6 +10,33 @@ function Sidebar() {
         <img src={logo} alt="Spotify" className="h-10" />
       </a>
       <Menu />
+      <nav className="mt-6">
+        <ul>
+          <li>
+            <a
+              href="#"
+              className="py-2 px-6 flex items-center group text-sm text-link font-semibold hover:text-white"
+            >
+              <span className="w-6 h-6 flex items-center justify-center mr-4 bg-white group-hover:bg-opacity-100 bg-opacity-60 rounded-sm text-black">
+                <Icon name="plus" size={12} />
+              </span>
+              Çalma Listesi Oluştur
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="py-2 px-6 flex items-center group text-sm text-link font-semibold hover:text-white"
+            >
+              <span className="w-6 h-6 flex items-center justify-center mr-4 opacity-70 bg-gradient-to-br from-purple-700 to-blue-300 text-white rounded-sm group-hover:bg-opacity-100">
+                <Icon name="heart" size={12} />
+              </span>
+              Beğenilen Şarkılar
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <Playlists />
     </div>
   );
 }

375b09becebffb957dd119837b0188ef3954bd07
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-09 17:16:43 +0300

    style: edited widecategory

--- src/components/views/Search.jsx
+++ src/components/views/Search.jsx
@@ -1,4 +1,6 @@
 import categories from "data/categories";
+import favoriteCategories from "data/favorite-categories";
+import Title from "components/Title";
 
 function Category({ category }) {
   return (
@@ -20,13 +22,46 @@ function Category({ category }) {
   );
 }
 
+function WideCategory({ category }) {
+  return (
+    <div
+      style={{ backgroundColor: category.color }}
+      className="rounded-lg relative w-[27.375rem] h-[13.75rem]"
+    >
+      <div className="absolute inset-0 overflow-hidden">
+        <h3 className="p-4 text-[2.5rem] tracking-tighter font-semibold">
+          {category.title}
+        </h3>
+        <img
+          src={category.cover}
+          alt={category.title}
+          className="w-32 h-32 rotate-[25deg] translate-x[18%] translate-y-[5%] absolute bottom-0 right-0 shadow-spotify"
+        />
+      </div>
+    </div>
+  );
+}
+
 function Search() {
   return (
-    <div className="grid grid-cols-5 gap-6">
-      {categories.map((category) => (
-        <Category category={category} />
-      ))}
-    </div>
+    <>
+      <section className="mb-4">
+        <Title title="En çok dinlediğin türler" />
+        <div>
+          {favoriteCategories.map((category, index) => (
+            <WideCategory key={index} category={category} />
+          ))}
+        </div>
+      </section>
+      <section>
+        <Title title="Hepsine göz at" />
+        <div className="grid grid-cols-5 gap-6">
+          {categories.map((category, index) => (
+            <Category key={index} category={category} />
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
 

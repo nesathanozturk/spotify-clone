82bda3f60b02f4e52ac27f5e1a447d0283ab1c5e
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-09 12:12:51 +0300

    style: edited player

--- src/components/BottomBar/Player.jsx
+++ src/components/BottomBar/Player.jsx
@@ -1,9 +1,27 @@
+import { Icon } from "Icons";
+
 function Player() {
   return (
     <div className="flex px-4 justify-between items-center h-full">
       <div className="mix-w-[11.25rem] w-[30%]">sol</div>
       <div className="flex flex-col items-center">
-        <div>üst</div>
+        <div className="flex gap-x-2 items-center">
+          <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
+            <Icon name="shuffle" size={16} />
+          </button>
+          <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
+            <Icon name="playerPrev" size={16} />
+          </button>
+          <button className="w-8 h-8 bg-white flex items-center justify-center text-black rounded-full hover:scale-[1.06]">
+            <Icon name="play" size={16} />
+          </button>
+          <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
+            <Icon name="playerNext" size={16} />
+          </button>
+          <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
+            <Icon name="repeat" size={16} />
+          </button>
+        </div>
         <div>alt</div>
       </div>
       <div className="mix-w-[11.25rem] w-[30%] flex justify-end">sağ</div>

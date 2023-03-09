c0990d9adca2994958c90eccdf268b46d5a2e9e1
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-09 23:15:25 +0300

    feat: added new playlist

--- src/components/Sidebar/Playlists.jsx
+++ src/components/Sidebar/Playlists.jsx
@@ -2,16 +2,30 @@ function Playlists() {
   return (
     <nav className="mx-6 mt-2 py-2 flex flex-auto border-t border-white border-opacity-20 overflow-auto">
       <ul>
-        {new Array(40).fill(
-          <li>
-            <a
-              href="#"
-              className="text-s text-link hover:text-white flex h-8 items-center"
-            >
-              Pop
-            </a>
-          </li>
-        )}
+        <li>
+          <a
+            href="#"
+            className="text-s text-link hover:text-white flex h-8 items-center"
+          >
+            GOAT
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="text-s text-link hover:text-white flex h-8 items-center"
+          >
+            Arctic Monkeys
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="text-s text-link hover:text-white flex h-8 items-center"
+          >
+            KALEO
+          </a>
+        </li>
       </ul>
     </nav>
   );

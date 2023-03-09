fe9c603ea07936c335ab65a28b6f9d53796b5e52
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-09 16:47:51 +0300

    style: edited search input

--- src/components/Navbar/Search.jsx
+++ src/components/Navbar/Search.jsx
@@ -1,5 +1,22 @@
+import { Icon } from "Icons";
+
 function Search() {
-  return <div>Search</div>;
+  return (
+    <div className="mr-auto ml-4 relative">
+      <label
+        htmlFor="search-input"
+        className="text-black w-10 h-10 flex items-center justify-center absolute top-0 left-0"
+      >
+        <Icon name="search" size={24} />
+      </label>
+      <input
+        type="text"
+        id="search-input"
+        className="max-w-full w-[22.75rem] pl-12 outline-none text-black h-10 bg-white rounded-3xl placeholder-black/50 text-sm"
+        placeholder="Sanatçılar, şarkılar veya podcast'ler"
+      />
+    </div>
+  );
 }
 
 export default Search;

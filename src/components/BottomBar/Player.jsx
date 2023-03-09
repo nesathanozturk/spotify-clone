9c8147c7a4d72f2ce722efb50b71d71d98ec994a
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-09 12:09:29 +0300

    feat: created player component

---
+++ src/components/BottomBar/Player.jsx
@@ -0,0 +1,14 @@
+function Player() {
+  return (
+    <div className="flex px-4 justify-between items-center h-full">
+      <div className="mix-w-[11.25rem] w-[30%]">sol</div>
+      <div className="flex flex-col items-center">
+        <div>üst</div>
+        <div>alt</div>
+      </div>
+      <div className="mix-w-[11.25rem] w-[30%] flex justify-end">sağ</div>
+    </div>
+  );
+}
+
+export default Player;

1260c7cf297bf4158c622107442ff8ffdbfbb584
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-09 22:54:04 +0300

    feat: added a element

--- src/components/Sidebar/DownloadApp.jsx
+++ src/components/Sidebar/DownloadApp.jsx
@@ -2,7 +2,10 @@ import { Icon } from "Icons";
 
 function DownloadApp() {
   return (
-    <a className="h-10 px-6 flex-shrink-0 text-sm font-semibold text-link hover:text-white flex items-center gap-x-4">
+    <a
+      href="#"
+      className="h-10 px-6 flex-shrink-0 text-sm font-semibold text-link hover:text-white flex items-center gap-x-4"
+    >
       <Icon name="download" size={20} />
       Uygulamayı Yükle
     </a>

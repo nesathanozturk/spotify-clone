e58a944df9b13cc6b46bcbe9b083fdaab481fda7
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-09 17:01:04 +0300

    feat: added spotif shadow

--- src/components/views/Search.jsx
+++ src/components/views/Search.jsx
@@ -1,5 +1,33 @@
+import categories from "data/categories";
+
+function Category({ category }) {
+  return (
+    <div
+      style={{ backgroundColor: category.color }}
+      className="rounded-md before:pt-[100%] before:block relative"
+    >
+      <div className="absolute inset-0 overflow-hidden">
+        <h3 className="p-4 text-2xl tracking-tighter font-semibold">
+          {category.title}
+        </h3>
+        <img
+          src={category.cover}
+          alt={category.title}
+          className="w-[6.25rem] h-[6.25rem] rotate-[25deg] translate-x[18%] translate-y-[5%] absolute bottom-0 right-0"
+        />
+      </div>
+    </div>
+  );
+}
+
 function Search() {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-5 gap-6">
+      {categories.map((category) => (
+        <Category category={category} />
+      ))}
+    </div>
+  );
 }
 
 export default Search;

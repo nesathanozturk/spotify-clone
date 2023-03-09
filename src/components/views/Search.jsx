329a222e2576a32209e9985ae79403c4f5ee8342
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-09 18:08:26 +0300

    feat:added scrols

--- src/components/views/Search.jsx
+++ src/components/views/Search.jsx
@@ -2,6 +2,8 @@ import categories from "data/categories";
 import favoriteCategories from "data/favorite-categories";
 import Title from "components/Title";
 import ScrollContainer from "react-indiana-drag-scroll";
+import { useEffect, useRef, useState } from "react";
+import { Icon } from "Icons";
 
 function Category({ category }) {
   return (
@@ -27,7 +29,7 @@ function WideCategory({ category }) {
   return (
     <div
       style={{ backgroundColor: category.color }}
-      className="rounded-lg relative w-[27.375rem] h-[13.75rem]"
+      className="rounded-lg flex-shrink-0 relative w-[27.375rem] h-[13.75rem]"
     >
       <div className="absolute inset-0 overflow-hidden">
         <h3 className="p-4 text-[2.5rem] tracking-tighter font-semibold">
@@ -44,15 +46,67 @@ function WideCategory({ category }) {
 }
 
 function Search() {
+  const favoritesRef = useRef();
+  const [prev, setPrev] = useState(false);
+  const [next, setNext] = useState(false);
+
+  useEffect(() => {
+    if (favoritesRef.current) {
+      const scrollHandle = () => {
+        const isEnd =
+          favoritesRef.current.scrollLeft + favoritesRef.current.offsetWidth ===
+          favoritesRef.current.scrollWidth;
+        const isBegin = favoritesRef.current.scrollLeft === 0;
+        setPrev(!isBegin);
+        setNext(!isEnd);
+      };
+
+      scrollHandle();
+      favoritesRef.current.addEventListener("scroll", scrollHandle);
+
+      return () => {
+        favoritesRef?.current?.removeEventListener("scroll", scrollHandle);
+      };
+    }
+  }, [favoritesRef]);
+
+  const slideFavoritesNext = () => {
+    favoritesRef.current.scrollLeft += favoritesRef.current.offsetWidth - 300;
+  };
+  const slideFavoritesPrev = () => {
+    favoritesRef.current.scrollLeft -= favoritesRef.current.offsetWidth - 300;
+  };
+
   return (
     <>
       <section className="mb-4">
         <Title title="En çok dinlediğin türler" />
-        <div>
+        <div className="relative">
+          {prev && (
+            <button
+              className="absolute -left-6 z-10 top-1/2 hover:scale-[1.06] -translate-y-1/2 w-12 h-12 rounded-full bg-white text-black flex justify-center items-center"
+              onClick={slideFavoritesPrev}
+            >
+              <Icon name="prev" size={24} />
+            </button>
+          )}
+          {next && (
+            <button
+              onClick={slideFavoritesNext}
+              className="absolute -right-6 z-10 top-1/2 hover:scale-[1.06] -translate-y-1/2 w-12 h-12 rounded-full bg-white text-black flex justify-center items-center"
+            >
+              <Icon name="next" size={24} />
+            </button>
+          )}
+        </div>
+        <ScrollContainer
+          innerRef={favoritesRef}
+          className="flex overflow-x gap-x-6 scrollable"
+        >
           {favoriteCategories.map((category, index) => (
             <WideCategory key={index} category={category} />
           ))}
-        </div>
+        </ScrollContainer>
       </section>
       <section>
         <Title title="Hepsine göz at" />
